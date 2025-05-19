97e2fec4f67dd48e1c8c5fca1202f08630b9b842
sarita1992 <1992saritachaudhary@gmail.com>
2025-05-19 17:12:41 +0300

    Initial Project Setup

---
+++ app/src/main/java/com/example/employerapplication/AndroidApp.kt
@@ -0,0 +1,12 @@
+package com.example.employerapplication
+
+import android.app.Application
+
+/**
+ * Create By sarita
+ */
+class AndroidApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
